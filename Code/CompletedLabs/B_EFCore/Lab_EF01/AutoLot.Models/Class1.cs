adee8dab82249f05c2be10600c092165e6565b33
Philip Japikse <japikse@outlook.com>
2025-09-08 11:47:08 -0400

    initial checkin

---
+++ Code/CompletedLabs/B_EFCore/Lab_EF01/AutoLot.Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿// Copyright Information
+// ==================================
+// AutoLot9 - AutoLot.Models - Class1.cs
+// All samples copyright Philip Japikse
+// http://www.skimedic.com 2025/09/02
+// ==================================
+
+namespace AutoLot.Models;
+
+public class Class1
+{
+
+}
---
+++ Code/CompletedLabs/C_RazorPages/Lab_RP07/AutoLot.Web/Pages/Error.cshtml.cs
@@ -0,0 +1,25 @@
+// Copyright Information
+// ==================================
+// AutoLot9 - AutoLot.Web - Error.cshtml.cs
+// All samples copyright Philip Japikse
+// http://www.skimedic.com 2025/09/02
+// ==================================
+
+namespace AutoLot.Web.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    private readonly ILogger<ErrorModel> _logger = logger;
+
+    public void OnGet()
+    {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+    }
+}
+
